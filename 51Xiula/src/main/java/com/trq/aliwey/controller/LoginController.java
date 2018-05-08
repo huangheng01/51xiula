de2f02ccd294261c26b48fed0561a893eb17b150
huangheng <stenju@163.com>
2018-05-08 11:12:10 +0800

    登录提交

--- 51Xiula/src/main/java/com/trq/aliwey/controller/LoginController.java
+++ 51Xiula/src/main/java/com/trq/aliwey/controller/LoginController.java
@@ -30,6 +30,6 @@ public class LoginController extends BaseController {
 		this.display();
 	}
 	public void login(){
-		System.out.println("sdfsf354354");
+		System.out.println("sdfsf3543542222");
 	}
 }
