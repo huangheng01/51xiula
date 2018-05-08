3efa997ed44c0cecb8631057702178db9c05658d
huangheng01 <38902810+huangheng01@users.noreply.github.com>
2018-05-08 11:09:53 +0800

    Update LoginController.java
    22

--- 51Xiula/src/main/java/com/trq/aliwey/controller/LoginController.java
+++ 51Xiula/src/main/java/com/trq/aliwey/controller/LoginController.java
@@ -30,6 +30,6 @@ public class LoginController extends BaseController {
 		this.display();
 	}
 	public void login(){
-		System.out.println("sdfsf354354");
+		System.out.println("sdfsf354354345435");
 	}
 }
