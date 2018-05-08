2bf3fa607a49948ee019819dd2640fee6359be0a
huangheng <stenju@163.com>
2018-05-08 10:07:57 +0800

    优化登录控制器代码

--- 51Xiula/src/main/java/com/trq/aliwey/controller/LoginController.java
+++ 51Xiula/src/main/java/com/trq/aliwey/controller/LoginController.java
@@ -1,24 +1,11 @@
 package com.trq.aliwey.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
-import com.alibaba.fastjson.JSONObject;
 import com.jranuan.aop.Clear;
 import com.jranuan.core.ActionKey;
-import com.jranuan.ext.kit.ConfigPropKit;
-import com.jranuan.ext.kit.TimeKit;
 import com.jranuan.ext.route.ControllerBind;
-import com.jranuan.kit.StrKit;
 import com.trq.aliwey.controller.BaseController;
 import com.trq.aliwey.interceptor.LoginInterceptor;
-import com.trq.aliwey.utils.StringTools;
-import com.trq.aliwey.utils.ZhimaApi;
 
 /**
  * 
@@ -31,10 +18,6 @@ import com.trq.aliwey.utils.ZhimaApi;
  * 
  * [环 境] JavaSE 1.7
  * 
- * @author tan.p
- * @date 2016年4月6日上午11:14:15
- * @version 1.0
- * </pre>
  */
 @Clear({ LoginInterceptor.class })
 @ControllerBind(path = "login")
